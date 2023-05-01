f741177e689bbe3e8bfaf73416a39489b62b782b
zfechter88 <99442701+zfechter88@users.noreply.github.com>
2023-05-01 23:48:12 +0000

    index

---
+++ index.js
@@ -0,0 +1,9 @@
+let player_score;
+
+
+
+function get_city(){
+    document.getElementById('city').innerHTML = 'Hong Kong'
+}
+
+document.getElementById('get_city_button').addEventListener('click', get_city);
